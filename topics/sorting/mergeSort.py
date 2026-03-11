cc1aad7b3aa3944ce15f428cae08899b63aec635
akshay yadav <akshayhat@gmail.com>
2026-03-11 19:17:16 +0530

    merge sort

---
+++ topics/sorting/mergeSort.py
@@ -0,0 +1,50 @@
+nums = list(map(int, input("Enter the array: ").split()))
+# [2, 3, 4, 1, 5]
+
+
+def merge_arrays(left, right):
+    result = []
+
+    i = 0
+    j = 0
+
+    left_len = len(left)
+    right_len = len(right)
+
+    while i < left_len and j < right_len:
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    if i < left_len:
+        while i < left_len:
+            result.append(left[i])
+            i += 1
+
+    if j < right_len:
+        while j < right_len:
+            result.append(right[j])
+            j += 1
+
+    return result
+
+
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left_part = arr[:mid]
+    right_part = arr[mid:]
+
+    left = mergeSort(left_part)
+    right = mergeSort(right_part)
+
+    return merge_arrays(left, right)
+
+
+print("The sorted array is: ", mergeSort(nums))
